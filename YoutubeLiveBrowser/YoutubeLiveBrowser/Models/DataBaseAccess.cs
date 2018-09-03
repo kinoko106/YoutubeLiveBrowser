0619798c91b3394b18ab82dc2e9eda2e728deff7
daichi takada <03daichi28@gmail.com>
2018-09-04 00:25:30 +0900

    ignore

---
+++ YoutubeLiveBrowser/YoutubeLiveBrowser/Models/DataBaseAccess.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeLiveBrowser.Models
+{
+	public class DataBaseAccess
+	{
+		public SqlConnection SQLConnection { get; set; }
+
+		public DataBaseAccess()
+		{
+			//接続文字列の設定
+			SQLConnection.ConnectionString = ConfigurationManager.AppSettings["ConnectionString"];
+
+
+		}
+	}
+}
